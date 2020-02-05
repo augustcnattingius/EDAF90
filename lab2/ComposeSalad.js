60c2cae3de0ee0c75fac839e05bd29a8ec5c61ff
augustcnattingius <august.cnattingius@live.se>
2020-02-05 13:10:58 +0100

    Setup complete

---
+++ lab2/ComposeSalad.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class ComposeSalad extends React.Component {
+  render() {
+    const inventory = this.props.inventory;
+    // test for correct ussage, the parent must send this datastructure
+    if (!inventory) {
+      alert("inventory is undefined in ComposeSalad");
+    }
+    let foundations = Object.keys(inventory).filter(
+      name => inventory[name].foundation
+    );
+    return (
+      <div className="container">
+        <h4>Välj bas</h4>
+        <ul>
+          {foundations.map(name => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ComposeSalad;
---
+++ lab2/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import $ from "jquery"; // skip this if you do not use bootstrap modals
+import Popper from "popper.js"; // skip this if you do not use bootstrap modals
+
+import "./styles.css";
+import inventory from "./inventory.ES6";
+import ComposeSalad from "./ComposeSalad";
+import ComposeSaladModal from "./ComposeSaladModal";
+
+function App() {
+  return (
+    <div>
+      <div className="jumbotron text-center">
+        <h1 className="display-4">EDAF90 - Web Programming</h1>
+        <p className="lead">
+          This is a template project for react + router + bootstrap.
+        </p>
+        <hr className="my-4" />
+        <p>This code is a good starting point for lab 2.</p>
+      </div>
+
+      <ComposeSaladModal inventory={inventory} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
